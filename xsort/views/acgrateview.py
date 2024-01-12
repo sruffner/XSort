9d7ef92dc1fe44b1d1f0ad3496019a0b28ff8e45
sar52 <sruffner@srscicomp.com>
2024-01-12 15:08:27 -0800

    Updated ACGRateView to render ACG-vs-firing rate histograms as heatmap images.

--- xsort/views/acgrateview.py
+++ xsort/views/acgrateview.py
@@ -4,7 +4,6 @@ from PySide6.QtCore import Qt, Slot, QSettings
 from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QSlider
 import pyqtgraph as pg
-import numpy as np
 from pyqtgraph import ViewBox
 
 from xsort.data.analyzer import Analyzer
@@ -18,12 +17,10 @@ class ACGRateView(BaseView):
     the unit's autocorrelogram as a function of instantaneous firing rate.
         The sampled firing rates (as observed at each spike occurrence) are distributed across 10 bins, and the
     autocorrelograms for each firing rate bin span the time range [-100 .. 100] milliseconds. These parameters cannot be
-    changed by the user. The series of 10 ACGs are plotted as separate curves, using the opaque color assigned to the
-    unit's position in the display list, but the alpha component increases from 64 to 255 with increasing firing rate.
-    The title of each subplot indicates the firing rate range covered by the ACGs.
+    changed by the user. The series of 10 ACGs are plotted as a heatmap image with 10 rows and 201 columns.
         The neuron display list, managed by :class:`Analyzer`, may contain up to 3 different neural units. This view
     renders a single row of up to 3 subplots, with the ACG-vs-firing-rate histogram for each unit in the list in a
-    separate subplot.
+    separate subplot. The subplot title reflects the unit UID and the range of firing rates spanned along the Y-axis.
         The ACG-vs-rate historgrams are computed lazily as needed, then cached in the :class:`Neuron` instances. This
     view is refreshed whenever the ACG-vs-rate histogram of a unit in the current display list is updated.
         Finally, a slider at the bottom of the view allows the user to change the visible correlogram span of all
@@ -37,6 +34,10 @@ class ACGRateView(BaseView):
     """ Maximum time span of the ACG-vs-firing rate histograms displayed in this view. """
     _PEN_WIDTH: int = 3
     """ Width of pen used to draw histograms. """
+    _COLORMAP: str = 'viridis'
+    """ The PyQtGraph colormap used to render the 3D histograms as heatmaps. """
+    _TITLE_FONT_SZ: str = '14pt'
+    """ Sets font size of plot title which displays range of firing rates for the ACG-vs-FR histogram in that plot. """
 
     def __init__(self, data_manager: Analyzer) -> None:
         super().__init__('ACG-vs-Firing Rate', None, data_manager)
@@ -94,7 +95,7 @@ class ACGRateView(BaseView):
         num_units = len(displayed)
         span_ms = Neuron.ACG_VS_RATE_SPAN_MS
         visible_span = self._hist_span_slider.sliderPosition()
-        for i, unit in enumerate(displayed):
+        for i in range(len(displayed)):
             # create the subplot item
             pi: pg.PlotItem = self._layout_widget.addPlot(row=0, col=i)
             pi.setMenuEnabled(False)
@@ -109,18 +110,18 @@ class ACGRateView(BaseView):
             color = QColor.fromString(Analyzer.FOCUS_NEURON_COLORS[i])
             rate_bins, acgs = displayed[i].cached_acg_vs_rate()
             if len(rate_bins) > 0:
-                x = np.linspace(start=-span_ms, stop=span_ms, num=acgs.shape[1])
-                for j in range(len(rate_bins)):
-                    color.setAlpha(min(255, 64 + int((j+1)*192/len(rate_bins))))
-                    pen = pg.mkPen(color=color, width=self._PEN_WIDTH)
-                    pi.plot(x=x, y=acgs[j, :], pen=pen, clear=False)
-                title = f"Firing rate: {rate_bins[0]:.1f}-{rate_bins[-1]:.1f} Hz"
-                pi.setTitle(title, color=color, bold=True)
+                n_bins = len(rate_bins)
+                rate_bin_size = (rate_bins[-1] - rate_bins[0]) / n_bins
+                img_rect = [-span_ms, rate_bins[0] - rate_bin_size/2.0, 2.0*span_ms, rate_bin_size*n_bins]
+                img = pg.ImageItem(image=acgs, axisOrder='row-major', rect=img_rect, colorMap=self._COLORMAP)
+                pi.addItem(img)
+                title = f"Unit {displayed[i].uid} : {rate_bins[0]:.1f}-{rate_bins[-1]:.1f} Hz"
+                pi.setTitle(title, color=color, bold=True, size=self._TITLE_FONT_SZ)
 
             if i == num_units - 1:
                 self._scale_bar = pg.ScaleBar(size=20, suffix='ms')
                 self._scale_bar.setParentItem(pi.getViewBox())
-                self._scale_bar.anchor(itemPos=(0, 1), parentPos=(0.75, 1), offset=(0, -20))
+                self._scale_bar.anchor(itemPos=(1, 1), parentPos=(1, 1), offset=(-10, -10))
 
     def on_working_directory_changed(self) -> None:
         self._reset()
@@ -128,7 +129,7 @@ class ACGRateView(BaseView):
     def on_focus_neurons_changed(self) -> None:
         self._reset()
 
-    def on_focus_neurons_stats_updated(self, data_type: DataType, unit_label: str) -> None:
+    def on_focus_neurons_stats_updated(self, data_type: DataType, uid: str) -> None:
         """
         Whenever the ACG-vs-rate histogram is updated for a neural unit in the current display list, we need to update
         the corresponding plot data item within the subplots currently installed in this view.
@@ -137,20 +138,19 @@ class ACGRateView(BaseView):
             return
         displayed = self.data_manager.neurons_with_display_focus
         span_ms = Neuron.FIXED_HIST_SPAN_MS
-        unit: Neuron
-        for i, unit in enumerate(displayed):
-            if unit.uid == unit_label:
+        for i in range(len(displayed)):
+            if displayed[i].uid == uid:
                 plot_item: pg.PlotItem = self._layout_widget.ci.getItem(row=0, col=i)
                 color = QColor.fromString(Analyzer.FOCUS_NEURON_COLORS[i])
                 rate_bins, acgs = displayed[i].cached_acg_vs_rate()
                 if len(rate_bins) > 0:
-                    x = np.linspace(start=-span_ms, stop=span_ms, num=acgs.shape[1])
-                    for j in range(len(rate_bins)):
-                        color.setAlpha(min(255, 64 + int((j + 1) * 192 / len(rate_bins))))
-                        pen = pg.mkPen(color=color, width=self._PEN_WIDTH)
-                        plot_item.plot(x=x, y=acgs[j, :], pen=pen, clear=False)
-                    title = f"Firing rate: {rate_bins[0]:.1f}-{rate_bins[-1]:.1f} Hz"
-                    plot_item.setTitle(title, color=color, bold=True)
+                    n_bins = len(rate_bins)
+                    rate_bin_size = (rate_bins[-1] - rate_bins[0]) / n_bins
+                    img_rect = [-span_ms, rate_bins[0] - rate_bin_size / 2.0, 2.0 * span_ms, rate_bin_size * n_bins]
+                    img = pg.ImageItem(image=acgs, axisOrder='row-major', rect=img_rect, colorMap=self._COLORMAP)
+                    plot_item.addItem(img)
+                    title = f"Unit {displayed[i].uid} : {rate_bins[0]:.1f}-{rate_bins[-1]:.1f} Hz"
+                    plot_item.setTitle(title, color=color, bold=True,  size=self._TITLE_FONT_SZ)
 
     @Slot()
     def _on_hist_span_changed(self):
